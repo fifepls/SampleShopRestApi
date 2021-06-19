9b58e76fa13386cf781597ba0945bde8b64d4038
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-19 15:21:06 +0300

    initial commit

---
+++ src/test/java/com/shop/core/core/ShopCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.core.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
