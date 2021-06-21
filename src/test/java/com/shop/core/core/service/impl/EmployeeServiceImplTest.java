fe80d7333758d1eab04968c773cb0d5ea747ede3
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-21 20:10:05 +0300

    added tests

--- src/test/java/com/shop/core/core/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/shop/core/core/service/impl/EmployeeServiceImplTest.java
@@ -1,4 +1,111 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.shop.core.core.service.impl;
+
+import com.shop.core.core.entity.Employee;
+import com.shop.core.core.entity.Shop;
+import com.shop.core.core.repository.EmployeeRepository;
+import com.shop.core.core.repository.ShopRepository;
+import com.shop.core.core.service.exception.employee.EmployeeNotAddedException;
+import com.shop.core.core.service.exception.employee.EmployeeNotRemovedException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class EmployeeServiceImplTest {
-  
+
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+
+    @MockBean
+    private EmployeeRepository employeeRepository;
+
+    @MockBean
+    private ShopRepository shopRepository;
+
+
+    //add new employee, phone is free
+    @Test
+    void addNewEmployee() throws EmployeeNotAddedException {
+    Employee newEmployee = new Employee();
+    newEmployee.setPhoneNumber("89999999999");
+    newEmployee.setName("name1");
+    newEmployee.setLastName("lastName1");
+    newEmployee.setHourlyPayment(new BigDecimal(500));
+    newEmployee.setId(1L);
+
+    Shop shop = new Shop();
+    shop.setAddress("shop adr");
+    shop.setId(1L);
+
+        Mockito.doReturn(Optional.of(shop)).when(shopRepository).findById(1L);
+        Mockito.doReturn(Optional.empty()).when(employeeRepository).findEmployeeByPhoneNumber(newEmployee.getPhoneNumber());
+        Mockito.doReturn(newEmployee).when(employeeRepository).save(newEmployee);
+        Employee addedEmployee = employeeService.addNewEmployee(newEmployee,shop.getId());
+
+        Assertions.assertEquals(newEmployee.getName(), addedEmployee.getName());
+        Assertions.assertEquals(newEmployee.getPhoneNumber(), addedEmployee.getPhoneNumber());
+        Assertions.assertEquals(newEmployee.getLastName(), addedEmployee.getLastName());
+        Assertions.assertEquals(newEmployee.getId(), addedEmployee.getId());
+        Assertions.assertEquals(newEmployee.getHourlyPayment(), newEmployee.getHourlyPayment());
+        Assertions.assertEquals(newEmployee.getShop(), newEmployee.getShop());
+
+        Mockito.verify(shopRepository,Mockito.times(1)).findById(1L);
+        Mockito.verify(employeeRepository,Mockito.times(1)).findEmployeeByPhoneNumber(newEmployee.getPhoneNumber());
+        Mockito.verify(employeeRepository,Mockito.times(1)).save(newEmployee);
+    }
+
+    @Test
+    void removeEmployee() throws EmployeeNotRemovedException {
+        Employee newEmployee = new Employee();
+        newEmployee.setPhoneNumber("89999999999");
+        newEmployee.setName("name");
+        newEmployee.setLastName("lastName");
+        newEmployee.setHourlyPayment(new BigDecimal(500));
+        newEmployee.setId(1L);
+
+        Assertions.assertTrue(employeeService.removeEmployee(1L));
+        Mockito.verify(employeeRepository).deleteById(1L);
+
+    }
+
+    @Test
+    void getAllEmployees() {
+        Employee newEmployee = new Employee();
+        newEmployee.setPhoneNumber("89999999999");
+        newEmployee.setName("name");
+        newEmployee.setLastName("lastName");
+        newEmployee.setHourlyPayment(new BigDecimal(500));
+        newEmployee.setId(1L);
+
+        Employee newEmployee1 = new Employee();
+        newEmployee1.setPhoneNumber("88888888888");
+        newEmployee1.setName("name2");
+        newEmployee1.setLastName("lastName2");
+        newEmployee1.setHourlyPayment(new BigDecimal(500));
+        newEmployee1.setId(2L);
+
+        List<Employee> employeeList = Stream.of(newEmployee,newEmployee1).collect(Collectors.toList());
+
+        Mockito.doReturn(employeeList).when(employeeRepository).findAll();
+
+        List<Employee> employeesFromService = employeeService.getAllEmployees();
+
+        Mockito.verify(employeeRepository,Mockito.times(1)).findAll();
+        Assertions.assertEquals(employeeList,employeesFromService);
+    }
+
+    @Test
+    void updateEmployee() {
+    }
 }
--- src/test/java/com/shop/core/core/service/impl/ShopServiceImplTest.java
+++ src/test/java/com/shop/core/core/service/impl/ShopServiceImplTest.java
@@ -1,4 +1,75 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.shop.core.core.service.impl;
+
+import com.shop.core.core.entity.Shop;
+import com.shop.core.core.repository.ShopRepository;
+import com.shop.core.core.service.exception.shop.ShopNotAddedException;
+import com.shop.core.core.service.exception.shop.ShopNotRemovedException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class ShopServiceImplTest {
-  
+
+    @Autowired
+    private ShopServiceImpl shopService;
+
+    @MockBean
+    private ShopRepository shopRepository;
+
+    @Test
+    void addNewShop() throws ShopNotAddedException {
+        Shop shop = new Shop();
+        shop.setAddress("some test adr");
+        shop.setId(1L);
+
+        Mockito.doReturn(shop).when(shopRepository).save(shop);
+
+        //invoking service method that should be tested
+        shopService.addNewShop(shop);
+
+        Mockito.verify(shopRepository,Mockito.times(1)).save(shop);
+        Assertions.assertEquals("some test adr", shop.getAddress());
+        Assertions.assertEquals(1L, shop.getId());
+
+    }
+
+    @Test
+    void removeShop() throws ShopNotRemovedException {
+        Shop shop1 = new Shop();
+        shop1.setAddress("adr1");
+        shop1.setId(1L);
+
+        //invoking service method that should be tested
+        Assertions.assertTrue(shopService.removeShop(1L));
+        Mockito.verify(shopRepository).deleteById(1L);
+    }
+
+    @Test
+    void getAllShops() {
+        List<Shop> shops = new ArrayList<>();
+        Shop shop1 = new Shop();
+        shop1.setAddress("adr1");
+        shop1.setId(1L);
+        Shop shop2 = new Shop() ;
+        shop1.setAddress("adr2");
+        shop1.setId(2L);
+        shops.add(shop1);
+        shops.add(shop2);
+
+        Mockito.doReturn(shops).when(shopRepository).findAll();
+
+        //invoking service method that should be tested
+        Assertions.assertEquals(shops, shopService.getAllShops());
+        Mockito.verify(shopRepository,Mockito.times(1)).findAll();
+
+    }
 }
